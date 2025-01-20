fa600d676067201a26648d6893b27dcafbba905a
Günter Ansinn <guenteransinn@gmail.com>
2025-01-19 23:35:44 -0500

    repo re-ordering Plus tests Adjusted: - Name of png test folder to have no space
    Added:
    - Tests for loading images no processing

--- src/main/java/net/ansinn/pixelatte/TextureLoader.java
+++ src/main/java/net/ansinn/pixelatte/TextureLoader.java
@@ -2,11 +2,14 @@ package net.ansinn.pixelatte;
 
 import net.ansinn.pixelatte.formats.png.PNGParser;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.HexFormat;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 public final class TextureLoader {
@@ -16,29 +19,6 @@ public final class TextureLoader {
 
     private TextureLoader() {}
 
-    /**
-     * Standard image parsing function. Given any unspecified file it will try to parse out what it is and what image format
-     * it belongs to. However, if you have a guarantee of what image formats you're feeding to these methods you may
-     * instead refer to the individual pre-made functions instead.
-     * <p></p>
-     * <p>[!] This method assumes you're starting at the beginning of an InputStream</p>
-     * @param input the data input stream containing the image
-     * @return an intermediary representation of an image. This can be converted into different types manually for
-     * dealing with libraries.
-     * @throws IOException
-     */
-    public static IntermediaryImage LoadImage(InputStream input) throws IOException {
-        // Check if formats list is empty, if it is throw an error because it shouldn't be.
-        if (FormatMap.isEmpty())
-            throw new IllegalStateException("No formats exist within the image format registry.");
-
-        var reading = true;
-        var availableNums = FormatMap.keySet();
-
-        // TODO complete this later.
-
-        return null;
-    }
 
     /**
      * Register a parsable image format and its according image processing function. Explicit collision rules must be
@@ -59,6 +39,25 @@ public final class TextureLoader {
     private static byte[] toHex(String hex) {
         return format.parseHex(hex.replace(" ",""));
     }
+
+    /**
+     * Reads a file from a given path and then prints the resulting data alongside how long it took.
+     * @param file file to memory map and quickly read
+     */
+    public static IntermediaryImage readFile(File file) {
+        try(var channel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+            var mbb = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+
+            
+
+        } catch (IOException exception) {
+            System.out.println("Error loading file: " + file.toPath() + " into channel.");
+            exception.printStackTrace();
+        }
+
+        return new IntermediaryImage();
+    }
+
 
     static {
         registerFormat(toHex("89 50 4E 47 0D 0A 1A 0A"), PNGParser::parse, CollisionRule.IGNORE);
