40f751bc4e74ddf9bbdbbb63363183736f78e1f5
Günter Ansinn <guenteransinn@gmail.com>
2026-02-24 18:57:07 -0500

    Cleanup for future:  Refactored:  - All example images into a PNG specific folder  Removed:  - DecodedImage.java  - DecodedImage8.java  - DecodedImage16.java

--- src/main/java/net/ansinn/pixelatte/output/DecodedImage16.java
+++
@@ -1,6 +0,0 @@
-package net.ansinn.pixelatte.output;
-
-import net.ansinn.pixelatte.formats.png.layout.ChunkMap;
-
-public record DecodedImage16(int width, int height, short[] pixels, Format format, ChunkMap chunkMap) implements DecodedImage {
-}
